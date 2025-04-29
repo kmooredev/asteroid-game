a7b723f7afee0e4dd802c80cd495ebc0d93ed6d2
kmooredev <kmooredev@gmail.com>
2025-04-28 19:56:44 -0500

    player and asteroid fields appear on screen

--- main.py
+++ main.py
@@ -1,19 +1,41 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+from asteroidfield import AsteroidField
 import pygame
+from player import Player
 from constants import *
+from asteroid import Asteroid
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    asteroids_field = AsteroidField()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        updatable.update(dt)
         screen.fill("black")
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
